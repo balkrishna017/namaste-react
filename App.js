8c8edbbc9e1486c23bc340ca8b700fe34f11a8b5
balkrishna017 <balkrishna017@gmail.com>
2023-01-02 17:47:01 +0530

    third lecture

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 /*
+GIT:   git push --set-upstream origin third-lecture
+
 HOT MODULE RELOAD USING PARCEL
 USING FILE WATCHER ALGORITHM--C++
 BUNDLING
@@ -31,24 +33,49 @@ const heading = React.createElement(
   },
   'Namaste Everyone from React'
 );
-
-const heading2 = React.createElement(
-  'h2',
-  {
-    id: 'title',
-    key: 'h2',
-  },
-  'Hello world'
+//  React.createElement gives => Object => HTML(and put up on DOM)
+// const heading2 = React.createElement(
+//   'h2',
+//   {
+//     id: 'title',
+//     key: 'h2',
+//   },
+//   'Hello world'
+// );
+// below code is not HTML inside javascript, it is JSX which is a HTML like syntax
+// in javascript
+const heading3 = (
+  <h1 id='title' key='h2'>
+    Namaste React JS
+  </h1>
 );
 
-const container = React.createElement(
-  'div',
-  {
-    id: 'container',
-  },
-  [heading, heading2]
+// const container = React.createElement(
+//   'div',
+//   {
+//     id: 'container',
+//   },
+//   [heading]
+// );
+
+// good practise to start with Capital letter
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading3}
+      <h1>Hello</h1>
+      <h2>Helo 2</h2>
+    </div>
+  );
+};
+
+const HeaderComponent2 = () => (
+  <div>
+    <h1>Hello</h1>
+    <h2>Helo 2</h2>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // passing a react element to root
-root.render(container);
+root.render(<HeaderComponent />);
